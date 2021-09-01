94a630deb2bcabc5afadbeb17b666e0beb4c1c17
simon Andersson <simon.1.andersson@hotmail.com>
2021-09-01 16:52:20 +0200

    new

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
-docker build -t simoande/multi-client:latest -t simoande/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t simoande/multi-server:latest -t simoande/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t simoande/multi-worker:latest -t simoande/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push simoande/multi-client:latest    
-docker push simoande/multi-server:latest
-docker push simoande/multi-worker:latest
+docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push simoande/multi-client:$SHA   
-docker push simoande/multi-server:$SHA
-docker push simoande/multi-worker:$SHA
+docker push cygnetops/multi-client-k8s:latest
+docker push cygnetops/multi-server-k8s-pgfix:latest
+docker push cygnetops/multi-worker-k8s:latest
+
+docker push cygnetops/multi-client-k8s:$SHA
+docker push cygnetops/multi-server-k8s-pgfix:$SHA
+docker push cygnetops/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=simoande/multi-server:$SHA
-kubectl set image deployments/client-deployment client=simoande/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=simoande/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
